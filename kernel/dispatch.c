c1244d6e157d66e0f891bbe45934c1ffc0da9226
Jonathan Olson <tojonol@gmail.com>
2015-10-14 19:17:59 -0700

    fixinshed hw3

--- kernel/dispatch.c
+++ kernel/dispatch.c
@@ -11,9 +11,7 @@ PROCESS active_proc;
  * Ready queues for all eight priorities.
  */
 PCB *ready_queue [MAX_READY_QUEUES];
-
-
-
+int ready_processes[MAX_READY_QUEUES];
 
 /*
  * add_ready_queue
@@ -21,9 +19,29 @@ PCB *ready_queue [MAX_READY_QUEUES];
  * The process pointed to by p is put the ready queue.
  * The appropiate ready queue is determined by p->priority.
  */
-
+//ASSIGNMENT 3
 void add_ready_queue (PROCESS proc)
 {
+    //if there is nothing in the ready queue at a given priority
+    //add the process at that level and set pointers to self
+    if (ready_queue[proc->priority] == NULL)
+    {
+	ready_queue[proc->priority] = proc;
+	proc->next = proc;
+	proc->prev = proc;
+        ready_processes[proc->priority]=1;
+    }
+    //otherwise there is a process there
+    //add the process before the existing process
+    //increase the count of processes ready at that level
+    else
+    {
+	proc->next = ready_queue[proc->priority];
+	proc->prev = ready_queue[proc->priority]->prev;
+	ready_queue[proc->priority]->prev->next = proc;
+	ready_queue[proc->priority]->prev = proc;
+        ready_processes[proc->priority]= 1+ ready_processes[proc->priority];
+    }
 }
 
 
@@ -34,12 +52,24 @@ void add_ready_queue (PROCESS proc)
  * The process pointed to by p is dequeued from the ready
  * queue.
  */
-
+//ASSIGNMENT 3
 void remove_ready_queue (PROCESS proc)
 {
+    //if it is the only item in the ready_queue
+    //remove it
+    if (proc->next == proc)
+    {
+	ready_queue[proc->priority] = NULL;
+	ready_processes[proc->priority] = 0;
+    }
+    //otherwise we clean up the pointers removing the process
+    else
+    {
+	ready_queue[proc->priority] = proc->next;
+	proc->next->prev = proc->prev;
+	proc->prev->next = proc->next;
+    }
 }
-
-
 
 /*
  * dispatcher
@@ -48,9 +78,19 @@ void remove_ready_queue (PROCESS proc)
  * with the highest priority is taken. Within one priority
  * level round robin is used.
  */
-
+//ASSIGNMENT 3
 PROCESS dispatcher()
 {
+    int i, j;
+    //go through each process, if there is a process at that priority
+    //and grab the highest priority queue's next process
+    for(j = MAX_READY_QUEUES-1; j > -1; j--)
+    {
+    	if (ready_processes[j] >= 1 )
+	{
+       	    return active_proc->next;
+	}
+    }
 }
 
 
@@ -63,18 +103,33 @@ PROCESS dispatcher()
  * The stack of the calling process is setup such that it
  * looks like an interrupt.
  */
+//ASSIGNMENT 4
 void resign()
 {
+	active_proc = dispatcher();
+	//check to make sure we retrieved something
+	check_active();
 }
 
-
+void check_active()
+{
+	assert(active_proc != NULL);
+}
 
 /*
  * init_dispatcher
  *----------------------------------------------------------------------------
  * Initializes the necessary data structures.
  */
-
+//ASSIGNMENT 3
 void init_dispatcher()
 {
+    int i;
+
+    for (i = 0; i < MAX_READY_QUEUES; i++)
+    {
+	ready_queue [i] = NULL;
+    	ready_processes[i] = 0;
+    }
+    add_ready_queue (active_proc);
 }
--- kernel/process.c
+++ kernel/process.c
@@ -1,37 +1,125 @@
-
 #include <kernel.h>
 
-
 PCB pcb[MAX_PROCS];
+PCB *next_proc_ptr;
 
+MEM_ADDR pushStack(MEM_ADDR esp, LONG ptr)
+{
+    esp = esp - 4;
+    poke_l (esp, ptr);
+    return esp;
+}
 
+//HOMEWORK 3
 PORT create_process (void (*ptr_to_new_proc) (PROCESS, PARAM),
 		     int prio,
 		     PARAM param,
 		     char *name)
 {
+    LONG default_value = 0;
+    MEM_ADDR     esp;
+    PROCESS      new_proc;
+    PORT         new_port;
+    new_proc = next_proc_ptr;
+    next_proc_ptr = new_proc->next;
+    new_proc->magic             = MAGIC_PCB;
+    new_proc->used              = TRUE;
+    new_proc->state             = STATE_READY;
+    new_proc->priority          = prio;
+    new_proc->first_port        = NULL;
+    new_proc->name              = name;
+
+    esp = 640 * 1024 - (new_proc - pcb) * 16 * 1024;
+
+    //PARAM
+    esp = pushStack(esp, (LONG)param);
+    //PROCESS
+    esp = pushStack(esp, (LONG)new_proc);
+    //RETURN ADDRESS
+    esp = pushStack(esp, default_value);
+    //ADDRESS OF NEW PROCESS
+    esp = pushStack(esp, (LONG)ptr_to_new_proc);
+    //EAX
+    esp = pushStack(esp, default_value);
+    //ECX
+    esp = pushStack(esp, default_value);
+    //EDX
+    esp = pushStack(esp, default_value);
+    //EBX
+    esp = pushStack(esp, default_value);
+    //EBP
+    esp = pushStack(esp, default_value);
+    //ESI
+    esp = pushStack(esp, default_value);
+    //EDI
+    esp = pushStack(esp, default_value);
+    new_proc->esp = esp;
+
+    add_ready_queue (new_proc);
+    return NULL;
 }
 
-
+//untouched
 PROCESS fork()
 {
     // Dummy return to make gcc happy
     return (PROCESS) NULL;
 }
 
-
-
-
+//HOMEWORK 3
 void print_process(WINDOW* wnd, PROCESS p)
 {
+    wprintf(wnd, "State      Active  Prio  Name\n");
+    wprintf(wnd, "-----------------------------------------------\n");
+
+    if (p == active_proc)
+        wprintf(wnd, "READY      *     %d %s\n", p->priority,p->name);
+    else
+        wprintf(wnd, "READY            %d %s\n", p->priority,p->name);
 }
 
+//HOMEWORK 3
 void print_all_processes(WINDOW* wnd)
 {
+    int i;
+    PCB* p = pcb;
+    wprintf(wnd, "State      Active  Prio  Name\n");
+    wprintf(wnd, "-----------------------------------------------\n");
+    for (i = 0; i < MAX_PROCS; i++)
+    {
+	if (p->used)
+	{
+	     if (p == active_proc)
+        	wprintf(wnd, "READY      *     %d %s\n", p->priority,p->name);
+    	     else
+        	wprintf(wnd, "READY            %d %s\n", p->priority,p->name);
+	     p++;
+	}
+    }
 }
 
-
-
+//HOMEWORK 3
 void init_process()
 {
+    int i;
+    //initialize the boot process
+    pcb[0].magic = MAGIC_PCB;
+    pcb[0].used = TRUE;
+    pcb[0].state = STATE_READY;
+    pcb[0].priority = 1;
+    pcb[0].first_port = NULL;
+    pcb[0].name = "Boot process";
+
+    for (i = 1; i < MAX_PROCS; i++)
+    {
+	pcb [i].magic = 0;
+	pcb [i].used = FALSE;
+	if (i == MAX_PROCS-1)
+	    pcb [i].next = NULL;
+	else
+	    pcb [i].next = &pcb [i+1];
+    }
+
+    next_proc_ptr = &pcb [1];
+    active_proc = pcb;
 }
