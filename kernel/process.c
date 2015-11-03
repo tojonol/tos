8844dd6c8aaef734beb6f9a6d9a6a5a1349e56bc
Jonathan Olson <tojonol@gmail.com>
2015-11-02 22:24:37 -0800

    fixed new process to create a new process with a new port

--- kernel/process.c
+++ kernel/process.c
@@ -31,6 +31,7 @@ PORT create_process (void (*ptr_to_new_proc) (PROCESS, PARAM),
     new_proc->first_port        = NULL;
     new_proc->name              = name;
 
+    new_port = create_new_port(new_proc);
     esp = 640 * 1024 - (new_proc - pcb) * 16 * 1024;
 
     //PARAM
@@ -58,7 +59,7 @@ PORT create_process (void (*ptr_to_new_proc) (PROCESS, PARAM),
     new_proc->esp = esp;
 
     add_ready_queue (new_proc);
-    return NULL;
+    return new_port;
 }
 
 //untouched
